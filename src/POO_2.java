6f2b1298842a91be1bd5790fcd23fde119ddd86e
Elizabeth145992 <e.cruzramirez@ugto.mx>
2026-08-25 18:07:35 -0600

    Refactor payment processing in POO_2: add BankTransfer class and update main method to utilize processPayment

--- src/POO_2.java
+++ src/POO_2.java
@@ -19,11 +19,21 @@ public class POO_2 {
         }
     }
 
-    public static void main(String[] args) {
-        PaymentMethod payment = new CreditCard();
-        payment.pay(100.0);
-        payment = new Cash();
-        payment.pay(50.0);
+    static class BankTransfer implements PaymentMethod {
+        @Override
+        public boolean pay(double amount) {
+            System.out.println("Pagando $" + amount + " mediante transferencia bancaria");
+            return true;
+        }
     }
 
+    public static boolean processPayment(PaymentMethod payment, double amount) {
+        return payment.pay(amount);
+    }
+
+    public static void main(String[] args) {
+        processPayment(new CreditCard(), 100);
+        processPayment(new Cash(), 200);
+        processPayment(new BankTransfer(), 300);
+    }
 }
