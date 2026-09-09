776e33ed167388ce07d26847048cd1e9fc711c3d
Elizabeth145992 <e.cruzramirez@ugto.mx>
2026-09-09 11:14:36 -0600

    Add validation and request classes for user and product creation, along with global exception handling

--- learning-spring-boot/src/main/java/com/elizabeth/learningspringboot/HelloController.java
+++ learning-spring-boot/src/main/java/com/elizabeth/learningspringboot/HelloController.java
@@ -1,16 +1,19 @@
 package com.elizabeth.learningspringboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hola Spring Boot!";
+    @GetMapping("/curso")
+    public String curso() {
+        return "Estoy aprendiendo Spring Boot";
     }
 
     @GetMapping("/saludo/{nombre}")
@@ -18,13 +21,30 @@ public class HelloController {
         return "Hola " + nombre;
     }
 
-    @GetMapping("/curso")
-    public String curso() {
-        return "Estoy aprendiendo Spring Boot";
-    }
-
     @GetMapping("/bienvenida")
     public String bienvenida(@RequestParam String nombre) {
         return "Bienvenida " + nombre;
+    }
+
+    @GetMapping("/usuario")
+    public String usuario(
+            @RequestParam String nombre,
+            @RequestParam int edad
+    ) {
+        return "Hola " + nombre + ", tienes " + edad + " años";
+    }
+
+    @PostMapping("/users")
+    public CreateUserRequest createUser(
+            @Valid @RequestBody CreateUserRequest request
+    ) {
+        return request;
+    }
+
+    @PostMapping("/products")
+    public  CreateProductRequest createProduct(
+            @Valid @RequestBody CreateProductRequest request
+    ) {
+        return request;
     }
 }
